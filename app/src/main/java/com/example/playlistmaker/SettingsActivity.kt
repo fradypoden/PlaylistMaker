067069f2a949b99dbfd1180b002c823510988abe
Denis <fradypoden@ya.ru>
2025-04-09 22:49:46 +0300

    Sprint 9 ДЗ 2. Реализация кнопок «Поделиться приложением», «Написать в поддержку», «Пользовательское соглашение» в окне настроек.
    Синий экран, hyperv, createChooser

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,5 +1,7 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -11,5 +13,41 @@ class SettingsActivity : AppCompatActivity() {
 
         val back = findViewById<TextView>(R.id.backButton)
         back.setOnClickListener { finish() }
+
+        val share = findViewById<TextView>(R.id.shareButton)
+        share.setOnClickListener { shareApp() }
+
+        val support = findViewById<TextView>(R.id.supportButton)
+        support.setOnClickListener { writeToSupport() }
+
+        val userAgreement = findViewById<TextView>(R.id.userAgreementButton)
+        userAgreement.setOnClickListener { userAgreement() }
     }
+
+    private fun shareApp() {
+        val intent = Intent(Intent.ACTION_SEND)
+        val text = "https://practicum.yandex.ru/android-developer/?from=catalog"
+        intent.putExtra(Intent.EXTRA_TEXT, text)
+        intent.setType("text/plain")
+        startActivity(Intent.createChooser(intent, "text"))
+    }
+
+    private fun writeToSupport() {
+        val subject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+        val text = "Спасибо разработчикам и разработчицам за крутое приложение!"
+        val intent = Intent(Intent.ACTION_SENDTO)
+        intent.data = Uri.parse("mailto:")
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("fradypoden@yandex.ru"))
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        intent.putExtra(Intent.EXTRA_TEXT, text)
+        startActivity(intent)
+    }
+
+    private fun userAgreement() {
+        val url = "https://yandex.ru/legal/practicum_offer/"
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        startActivity(intent)
+    }
+
 }
