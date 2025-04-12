d66e5c58b55eb45948e0f62be6c333bdc948fbbc
Denis <fradypoden@ya.ru>
2025-04-12 03:15:19 +0300

    Sprint 9 ДЗ 3. Добавлена реализация кнопки Х, добавлен textwatcher, скрытие клавиатуры

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,10 +1,19 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+    @SuppressLint("ServiceCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
@@ -12,7 +21,36 @@ class SearchActivity : AppCompatActivity() {
         val back = findViewById<TextView>(R.id.backButton)
         back.setOnClickListener { finish() }
 
+        val searchLine = findViewById<EditText>(R.id.searchLine)
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        val clearButton = findViewById<ImageView>(R.id.clearButton)
+        clearButton.setOnClickListener {
+            searchLine.setText("")
+            inputMethodManager?.hideSoftInputFromWindow(searchLine.windowToken, 0)
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+
+        searchLine.addTextChangedListener(simpleTextWatcher)
+
     }
 
-
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
 }
